2bf064ba40ffb1c9fc0dc8a126203dd96fd66b08
Camila Poloche Londoño <camila.polochel@autonoma.edu.co>
2025-05-21 12:24:03 -0500

    Solucionando error git

--- src/autonoma/menuJuegos/elements/Snake.java
+++ src/autonoma/menuJuegos/elements/Snake.java
@@ -13,65 +13,39 @@ import java.util.ArrayList;
  *
  * @author Camila
  */
-public class Snake extends SpriteContainer{
-    
-    Serpiente snakeHead;
-    ArrayList<Serpiente> snakeBody;
-    ComidaSnake food;
-    
+public class Snake{
+    public Tile head;
+    public ArrayList<Tile> body = new ArrayList<>();
+    public int velocityX = 1;
+    public int velocityY = 0;
 
-    public Snake(int x, int y) {
-        super(x, y);
-        this.snakeHead = new Serpiente(5,5);
-        this.snakeBody = new ArrayList<Serpiente>();
-        this.food = food;
+    public Snake(int startX, int startY) {
+        head = new Tile(startX, startY);
     }
 
-   
-     public void move() {
-         //eat food
-         if (collision(snakeHead, food)) {
-             snakeBody.add(new ComidaSnake(food.getX(), food.getY()));
-             placeFood();
-         }
- 
-         //move snake body
-         for (int i = snakeBody.size()-1; i >= 0; i--) {
-             Tile snakePart = snakeBody.get(i);
-             if (i == 0) { //right before the head
-                 snakePart.x = snakeHead.x;
-                 snakePart.y = snakeHead.y;
-             }
-             else {
-                 Tile prevSnakePart = snakeBody.get(i-1);
-                 snakePart.x = prevSnakePart.x;
-                 snakePart.y = prevSnakePart.y;
-             }
-         }
-         //move snake head
-         snakeHead.x += velocidadX;
-         snakeHead.y += velocidadY;
- 
-         //game over conditions
-         for (int i = 0; i < snakeBody.size(); i++) {
-             Tile snakePart = snakeBody.get(i);
- 
-             //collide with snake head
-             if (collision(snakeHead, snakePart)) {
-                 gameOver = true;
-             }
-         }
- 
-         if (snakeHead.x*tileSize < 0 || snakeHead.x*tileSize > boardWidth || //passed left border or right border
-             snakeHead.y*tileSize < 0 || snakeHead.y*tileSize > boardHeight ) { //passed top border or bottom border
-             gameOver = true;
-         }
-     }
- 
-     public boolean collision(Tile tile1, Tile tile2) {
-         return tile1.x == tile2.x && tile1.y == tile2.y;
-     }
+    public void move() {
+        if (!body.isEmpty()) {
+            for (int i = body.size() - 1; i > 0; i--) {
+                body.get(i).x = body.get(i - 1).x;
+                body.get(i).y = body.get(i - 1).y;
+            }
+            body.get(0).x = head.x;
+            body.get(0).y = head.y;
+        }
+        head.x += velocityX;
+        head.y += velocityY;
+    }
 
-    
-    
+    public void grow(Tile foodTile) {
+        body.add(new Tile(foodTile.x, foodTile.y));
+    }
+
+    public boolean collidesWithSelf() {
+        for (Tile t : body) {
+            if (t.x == head.x && t.y == head.y) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
