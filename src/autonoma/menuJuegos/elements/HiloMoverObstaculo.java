6ae857f932b529d0cbe22ca99b178f910340b3c7
marianaRivas02 <rivasclavijom12@gmail.com>
2025-05-24 12:02:41 -0500

    modificaciones juego flappyBird

---
+++ src/autonoma/menuJuegos/elements/HiloMoverObstaculo.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.menuJuegos.elements;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author maria
+ */
+public class HiloMoverObstaculo implements Runnable{
+
+    private FlappyBird flappyBird;
+    private boolean running = false;
+    private int velocidad = 250; 
+    private int ultimoNivel = 0;
+    private Sonido sonido;
+    
+    public HiloMoverObstaculo(FlappyBird flappyBird) {
+        this.flappyBird = flappyBird;
+        this.sonido = new Sonido();
+    }
+
+    @Override
+    public void run() {
+        running = true;
+        while (running) {
+            if (!flappyBird.isGameOver()) {
+//                this.sonido.reproducir("SoundSnake.wav");
+                flappyBird.moverObstaculos();
+                
+                int nivelActual = flappyBird.getPuntaje() / 10;
+                if (nivelActual > ultimoNivel) {
+                    velocidad -= 20;
+                    if (velocidad < 50) velocidad = 50;
+                    ultimoNivel = nivelActual;
+                }
+            } 
+            
+            try {
+                Thread.sleep(velocidad);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+    
+    public void start() {
+        running = true;
+        Thread hilo = new Thread(this);
+        hilo.start();
+    }
+
+    public void stop() {
+        running = false;
+    }
+    
+}
